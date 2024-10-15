fe1eabe3ce5272d1bb64501e7a0ac0a1d4f78329
Clemens Horn <clemens.horn@mni.thm.de>
2024-10-15 13:53:46 +0200

    fix: breaking change in moon query tasks

--- default.nix
+++ default.nix
@@ -2,7 +2,7 @@
 with pkgs;
   rustPlatform.buildRustPackage {
     pname = "lpi";
-    version = "0.1.0";
+    version = "0.1.1";
     src = ./.;
-    cargoHash = "sha256-t3j6SL6T08pQw5sE6ZBSjAjOM+9dk4Nw5LGRqnobCWY=";
+    cargoHash = "sha256-EwDsrGUt2bHOGkTTtwN8vyJv8PUwgHtTf1fEEH9PF3Y=";
   }
